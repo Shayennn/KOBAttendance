a050d348fb3af6ae7153b40da59c89bd3a13d341
Shayennn <me@shayennn.com>
2019-03-28 19:28:06 +0700

    Implement Search API with Frontend

--- KOBAttendance/components/SearchBox.vue
+++ KOBAttendance/components/SearchBox.vue
@@ -7,8 +7,65 @@
       header-text-variant="white"
     >
       <b-card-text>
-        <b-form-input placeholder="Keyword" autofocus />
+        <b-form-input v-model="keyword" placeholder="Keyword" autofocus />
       </b-card-text>
+      <b-list-group flush>
+        <b-list-group-item
+          v-for="row in result"
+          :key="row.StudentID"
+          href="#"
+          class="flex-column align-items-start"
+          @click="onSelectPerson(row)"
+        >
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{ row.Name }} {{ row.Surname }}</h5>
+          </div>
+
+          <p class="mb-1">{{ row.ENName }} {{ row.ENSurname }}</p>
+
+          <small class="text-muted">{{ row.StudentID }}</small>
+        </b-list-group-item>
+      </b-list-group>
     </b-card>
   </div>
 </template>
+
+<script>
+import _ from 'underscore'
+
+export default {
+  data: function() {
+    return {
+      keyword: '',
+      result: [],
+      selected: null
+    }
+  },
+  watch: {
+    keyword: _.debounce(function() {
+      this.SearchResult()
+    }, 500)
+  },
+  methods: {
+    SearchResult() {
+      if (this.keyword.length < 3) {
+        this.result = []
+      } else {
+        this.$http
+          .get('/student/search', {
+            params: {
+              keyword: this.keyword
+            }
+          })
+          .then(resp => {
+            this.result = resp.data.result
+          })
+      }
+    },
+    onSelectPerson(selected) {
+      this.selected = selected
+      this.$emit('selectperson', this.selected)
+    }
+  }
+}
+</script>
--- KOBAttendance/pages/index.vue
+++ KOBAttendance/pages/index.vue
@@ -8,7 +8,8 @@
         <UserInfo :info="info" />
       </b-col>
       <b-col md="4">
-        <SearchBox />
+        <p>{{ targetStudentID }}</p>
+        <SearchBox :target="targetStudentID" @selectperson="SearchConfirm" />
       </b-col>
     </b-row>
     <Footer />
@@ -32,17 +33,43 @@ export default {
   data() {
     return {
       info: {
-        img: 'http://bucroccs.bu.ac.th/image/person.png',
-        name: 'พิชวัชร',
-        surname: 'ลัคนาธิติ',
-        en_name: 'Phitchawat Lukkanathiti',
-        year: '1',
-        stdid: '6110503371',
-        dep: 'วิศวกรรมคอมพิวเตอร์'
-      }
+        img: '/unknow.png',
+        name: 'กรุณาระบุคำค้น',
+        surname: '',
+        en_name: ' ',
+        year: '',
+        stdid: '',
+        dep: ''
+      },
+      targetStudentID: ''
     }
   },
-  computed: {},
-  methods: {}
+  async created() {
+    await this.$http.get('/auth/check')
+  },
+  methods: {
+    SearchConfirm(SearchData) {
+      const thisyear = 61
+      this.info.img = '/api/student/image?id=' + SearchData.StudentID
+      this.info.stdid = SearchData.StudentID
+      this.info.name = SearchData.Name
+      this.info.surname = SearchData.Surname
+      this.info.en_name = SearchData.ENName + ' ' + SearchData.ENSurname
+      this.info.year =
+        thisyear - Math.floor(SearchData.StudentID / 100000000) + 1
+      this.$http
+        .get('/student/info', {
+          params: {
+            id: SearchData.StudentID
+          }
+        })
+        .then(resp => {
+          this.info.name = resp.data.thainame.split(' ')[1]
+          this.info.surname = resp.data.thainame.split(this.info.name + ' ')[1]
+          this.info.en_name = resp.data.engname
+          this.info.dep = resp.data['major-id']
+        })
+    }
+  }
 }
 </script>
